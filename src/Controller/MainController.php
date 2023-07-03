4b28bc221fb82e3e894e6bf4ef75b58c5625568c
lola <a@a.a>
2023-07-03 17:10:29 +0200

    Ajout du controller SecurityController

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -16,8 +16,14 @@ class MainController extends AbstractController
     #[Route('/', name: 'main_home')]
     public function home(): Response
     {
+
+
+
+
+
         return $this->render('main/home.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
+
 }
