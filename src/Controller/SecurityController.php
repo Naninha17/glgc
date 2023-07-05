08c777c3860e64dd7f06799fb07b98fb67ae45d9
lola <a@a.a>
2023-07-05 18:31:08 +0200

    Installation bundle EasyAdmin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -16,8 +17,8 @@ class SecurityController extends AbstractController
     #[Route(path:'/connexion/', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        //Si l'admin est déjà connecté, on le redirige vers la page d'accueil
         if ($this->getUser()){
-
             return $this->redirectToRoute('main_home');
         }
 
@@ -28,6 +29,8 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
+
 
     /**
      * Contrôleur de la page de déconnexion
