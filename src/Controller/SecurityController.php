f6c4b91b719cc1dd0fc651270aa3ccef1a084e2d
lola <a@a.a>
2023-07-07 17:18:30 +0200

    Ajout du composer Rate-Limiter

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,40 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * Contrôleur de la page de connexion
-     */
-
-    #[Route(path:'/connexion/', name: 'app_login')]
+    #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        //Si l'admin est déjà connecté, on le redirige vers la page d'accueil
-        if ($this->getUser()){
-            return $this->redirectToRoute('main_home');
-        }
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
 
         // get the login error if there is one
-        $error =$authenticationUtils->getLastAuthenticationError();
+        $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-
-
-    /**
-     * Contrôleur de la page de déconnexion
-     */
-    #[Route(path: '/deconnexion/', name: 'app_logout')]
+    #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-
 }
